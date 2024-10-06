4189e4a74c921447db8695389bf8467dbed53efb
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-10-06 05:05:54 -0400

    set up most tests for phase 3 specs

---
+++ phases/03-practice-problems.js
@@ -0,0 +1,34 @@
+const HashTable = require('./01-implementation');
+const MySet = require('./02-practice-problems');
+
+function kth(str, frequency) {
+
+}
+
+function newAlphabet() {
+
+}
+
+function longestPalindrome() {
+
+}
+
+function longestSubstr() {
+
+}
+
+function maxSubarr() {
+
+}
+
+function coinChange() {
+
+}
+
+function climbingSteps() {
+
+}
+
+module.exports = [kth, newAlphabet, longestPalindrome, longestSubstr,
+    maxSubarr, coinChange, climbingSteps
+];
--- test/03-practice-problems-spec.js
+++ test/03-practice-problems-spec.js
@@ -1,8 +1,8 @@
 // const { describe } = require('mocha');
-const { describe, it } = require('mocha');
-const { Kth, newAlphabet, longestPalindrome, longestSubstr, maxSubarr,
+// const { describe, it } = require('mocha');
+const [ kth, newAlphabet, longestPalindrome, longestSubstr, maxSubarr,
     coinChange, climbingSteps
-} = require('./03-practice-problems');
+ ] = require('../phases/03-practice-problems');
 
 const chai = require('chai');
 const expect = chai.expect;
@@ -12,7 +12,14 @@ describe('', function () {
     describe('Kth', function () {
 
         it('should return the kth most frequent character in the string', function () {
+            let str1 = 'aaabbc';
+            let frequency1 = 1;
+            let frequency2 = 2;
+            let frequency3 = 3;
 
+            expect(kth(str1, frequency1), 'to equal 1').to.equal(1);
+            expect(kth(str1, frequency2), 'to equal 2').to.equal(2);
+            expect(kth(str1, frequency3), 'to equal 3').to.equal(3);
         });
 
     });
@@ -20,19 +27,28 @@ describe('', function () {
     describe('newAlphabet', function () {
 
         it('determine whether the characters in the string appear in lexicographically increase order', function () {
+            let alpha = 'abcdefghijklmnopqrstuvwxyz';
+            let str1 = 'dino';
+            let str2 = 'leetcode';
+            let str3 = 'leetcod';
 
+            expect(newAlphabet(str1, alpha)).to.be.true;
+            expect(newAlphabet(str2, alpha)).to.be.false;
+            expect(newAlphabet(str3, alpha)).to.be.true;
         });
 
         it('solve in O(m + n) time', function () {
 
-        })
+        });
 
     });
 
     describe('longestPalindrome', function () {
 
         it('determine the length of the longest palindrone that can be built with those letters', function () {
+            let str = "abccccdd";
 
+            expect(longestPalindrome(str)).to.equal('dccaccd');
         });
 
         it('solve in O(n) Time', function () {
@@ -44,7 +60,11 @@ describe('', function () {
     describe('longestSubstr', function () {
 
         it('find the length of the longest substring without repeating characters', function () {
+            let str1 = 'abcabcbb';
+            let str2 = 'bbbbb';
 
+            expect(longestSubstr(str1)).to.equal('abc');
+            expect(longestSubstr(str2)).to.equal('b');
         });
 
         it('Solve in O(n) Time', function () {
@@ -56,7 +76,11 @@ describe('', function () {
     describe('maxSubarr', function () {
 
         it('return the length of the longest subarray where the difference between its maximun value and its minimun value is at most 1', function () {
+            let arr1 = [1,3,2,2,5,2,3,7];
+            let arr2 = [1,1,1,1,3];
 
+            expect(maxSubarr(arr1)).to.equal(5);
+            expect(maxSubarr(arr2)).to.equal(4);
         });
 
         it('Solve this O(n) Time', function () {
@@ -70,7 +94,9 @@ describe('', function () {
         context('If amount of money cannot be made up by any combinations of coins', function () {
 
             it('', function () {
+                let coins = [5];
 
+                expect(coinChange(coins, 3)).to.equal(-1)
             });
 
         });
@@ -78,7 +104,10 @@ describe('', function () {
         context('If amount of money can be made up by any combinations of coins', function () {
 
             it('', function () {
+                let coins = [1, 5, 10, 25];
 
+                expect(coinChange(coins, 11)).to.equal(2);
+                expect(coinChange(coins, 0)).to.equal(0);
             });
 
         });
@@ -88,7 +117,11 @@ describe('', function () {
     describe('climbingSteps', function () {
 
         it('return the number of ways an individual can take up to n steps', function () {
-
+            expect(climbingSteps(0)).to.equal(1);
+            expect(climbingSteps(1)).to.equal(1);
+            expect(climbingSteps(2)).to.equal(2);
+            expect(climbingSteps(3)).to.equal(4);
+            expect(climbingSteps(4)).to.equal(6);
         });
 
     });
