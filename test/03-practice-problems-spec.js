0608daea46e9bd716051eb26b5c5c2a40d6c153f
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-10-06 01:11:07 -0400

    setting up grondwork for tests

--- test/03-practice-problems-spec.js
+++ test/03-practice-problems-spec.js
@@ -11,34 +11,86 @@ describe('', function () {
 
     describe('Kth', function () {
 
-    });
-
-    describe('newAlphabet', function () {
-
         it('should return the kth most frequent character in the string', function () {
 
         });
 
     });
 
+    describe('newAlphabet', function () {
+
+        it('determine whether the characters in the string appear in lexicographically increase order', function () {
+
+        });
+
+        it('solve in O(m + n) time', function () {
+
+        })
+
+    });
+
     describe('longestPalindrome', function () {
+
+        it('determine the length of the longest palindrone that can be built with those letters', function () {
+
+        });
+
+        it('solve in O(n) Time', function () {
+
+        });
 
     });
 
     describe('longestSubstr', function () {
 
+        it('find the length of the longest substring without repeating characters', function () {
+
+        });
+
+        it('Solve in O(n) Time', function () {
+
+        });
+
     });
 
     describe('maxSubarr', function () {
+
+        it('return the length of the longest subarray where the difference between its maximun value and its minimun value is at most 1', function () {
+
+        });
+
+        it('Solve this O(n) Time', function () {
+
+        });
 
     });
 
     describe('coinChange', function () {
 
+        context('If amount of money cannot be made up by any combinations of coins', function () {
+
+            it('', function () {
+
+            });
+
+        });
+
+        context('If amount of money can be made up by any combinations of coins', function () {
+
+            it('', function () {
+
+            });
+
+        });
+
     });
 
     describe('climbingSteps', function () {
 
-    })
+        it('return the number of ways an individual can take up to n steps', function () {
+
+        });
+
+    });
 
 });
